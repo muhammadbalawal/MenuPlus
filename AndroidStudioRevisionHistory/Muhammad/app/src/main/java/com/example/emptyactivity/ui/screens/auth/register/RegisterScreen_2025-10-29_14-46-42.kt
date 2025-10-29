5793b01d8f5929ecda78d5d6eedcc7d77fd793f1
Muhammad Balawal Safdar <158082654+muhammadbalawal@users.noreply.github.com>
2025-10-29 15:26:35 -0400

    Landing page with new navigation (#6)
    * Refactored project structure and implemented new navigation
    
    Deleted old navigation, layout, and screen files.
    Introduced a new, type-safe navigation system using a sealed `Route` interface.
    Added new screens for landing, login, register, profile, and menu handling.
    Implemented a bottom navigation bar and updated the main app structure (`MenuPlusApp`).
    Updated Gradle dependencies and build configurations.
    
    * Fix trailing whitespaces and commas
    
    * Fix: Added trailing commas in BottomNavigationBar
    
    * completed file structure and also reviewed kt lint
    
    * added the old landing page with the new navigation
    
    ---------
    
    Co-authored-by: Malik Al <malik.shourbaji@gmail.com>

---
+++ AndroidStudioRevisionHistory/Muhammad/app/src/main/java/com/example/emptyactivity/ui/screens/auth/register/RegisterScreen_2025-10-29_14-46-42.kt
@@ -0,0 +1,12 @@
+package com.example.emptyactivity.ui.screens.auth.register
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterScreen(
+    onNavigateToLogin: () -> Unit,
+    onRegisterSuccess: () -> Unit,
+) {
+    Text(text = "Register")
+}
